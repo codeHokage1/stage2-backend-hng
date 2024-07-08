a4d3f9bfce1d808c85cc38e2779fea15c06257a1
Farhan Sodiq <sodiqfarhan360@gmail.com>
2024-07-08 15:31:38 +0100

    Add logic for unknown route

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const express = require('express');
+const express = require("express");
 require("dotenv").config();
 
-const apiRouter = require('./routes/apiRouter');
-const dbPool = require('./config/dbConfig');
+const apiRouter = require("./routes/apiRouter");
+const dbPool = require("./config/dbConfig");
 
 const app = express();
 
 app.use(express.json());
 
-app.use("/", apiRouter)
+app.use("/", apiRouter);
+app.use("*", (req, res) => {
+  return res.status(404).json({
+    status: "error",
+    error: "Route not found",
+  });
+});
 
 module.exports = app;
